d1305cf574b0b2f7c255ce910295efd5f1046f1f
Marcel Kapfer <opensource@mmk2410.org>
2023-07-18 23:35:27 +0200

    ✨ (flatpak-sync) Add script for declaring flatpak apps

---
+++ flatpak-sync/flatpak-sync.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+APPS_LIST_PATH="${APPS_LIST_PATH:=$HOME/.config/flatpak-sync/apps.list}"
+
+readarray -t requested_apps < <(cat "$APPS_LIST_PATH")
+readarray -t installed_apps < <(flatpak list --app --columns app | tail -n +1)
+
+declare -a requested_apps_cleaned=()
+
+echo "Checking for apps to install."
+for app in "${requested_apps[@]}"; do
+    if [[ -z "$app" ]] || [[ "$app" =~ ^#.* ]]; then
+        continue
+    fi
+    requested_apps_cleaned+=("$app")
+    if [[ ! "${installed_apps[*]}" =~ "${app}" ]]; then
+        echo "$app not installed. Installing it."
+        flatpak install -y "$app"
+    fi
+done
+
+echo "Checking for apps to remove."
+for app in "${installed_apps[@]}"; do
+    if [[ ! "${requested_apps_cleaned[*]}" =~ "${app}" ]]; then
+        echo "$app no found in apps list. Removing it.";
+        flatpak uninstall -y "$app"
+    fi
+done
